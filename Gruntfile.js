d31b77116a1b692f651a2226546f89d11abb73bf
cyryl91 <91.cyryl@gmail.com>
2017-04-19 19:40:54 +0200

    new task jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,10 +11,14 @@ module.exports = function(grunt) {
           'css/style.css': 'sass/style.sass'
         }
       }
+    },
+    jshint: {
+      all: ['js/*.js']
     }
   });
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   // Default task(s).
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass', 'jshint']);
 };
