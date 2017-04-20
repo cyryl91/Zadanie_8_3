95d60addf57c8e76c71f67941e2aa77035ff47dd
cyryl91 <91.cyryl@gmail.com>
2017-04-20 16:37:39 +0200

    task kodilla 8.4 coments

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// komentarz liniowy example
+
+/*
+alert('witaj');       komentarz blokowy example
+*/
+
+var name = prompt('Witaj, podaj swoje imię');
+alert('Twoje mie to, ' +name);
+//console.log(name);
