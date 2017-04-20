827736500b8bfb034ea87076e33e2dc1893d618a
cyryl91 <91.cyryl@gmail.com>
2017-04-20 20:36:19 +0200

    pole trojkąta

--- scripts.js
+++ scripts.js
@@ -3,7 +3,28 @@
 /*
 alert('witaj');       komentarz blokowy example
 */
-
-var name = prompt('Witaj, podaj swoje imię');
-alert('Twoje mie to, ' +name);
+//var ameba = 'Zielona ameba spotkała się z Neptunem w kawiarni "Kajko".';
+//alert(ameba);
 //console.log(name);
+/*
+var test = ['HTML', 5, 'World', 9.99, ['Lorem', 'Ipsum', 2]];
+var foo = test[4];
+   var bar = foo[1];
+*/
+/*
+var points = 3;
+
+console.log( 'bieżąca wartość zmiennej points to: ', points ); 
+console.log( 'bieżąca wartość zmiennej points to: ', ++points ); 
+console.log( 'bieżąca wartość zmiennej points to: ', points--); 
+console.log( 'bieżąca wartość zmiennej points to: ', points ); 
+*/
+
+
+var sideA = prompt('podaj bok a trójkąta ');
+	heightTriangle = prompt('podaj wysokośc trójkąta ');
+	triangleArea = (sideA*heightTriangle/2);
+
+
+alert('pole trójkąta o boku ' +sideA+ ' i wysokości ' +heightTriangle+ ' wynosi ' +triangleArea);
+console.log('pole trójkąta o boku ' +sideA+ ' i wysokości ' +heightTriangle+ ' wynosi ' +triangleArea);
